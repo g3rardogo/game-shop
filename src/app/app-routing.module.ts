4f3e91c1a8510d3eb5f08be89cb853d068be93ec
Gerardo Ramirez <gerardogo145@gmail.com>
2021-09-27 12:43:57 +0200

    :art: Get params in product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'products',
     component: ProductsComponent,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
   },
   {
     path: 'contact',
---
+++ src/app/products.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Product } from './interfaces/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/cyberpunk.jpg',
+      title: 'Cyberpunk',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '2',
+      image: 'assets/images/dead-space.jpg',
+      title: 'Dead Space',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '3',
+      image: 'assets/images/destiny.png',
+      title: 'Destiny',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '4',
+      image: 'assets/images/fifa-21.jpg',
+      title: 'FIFA 21',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '5',
+      image: 'assets/images/mortal-kombat.jpg',
+      title: 'Mortal Kombat',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+    {
+      id: '6',
+      image: 'assets/images/spiderman.jpg',
+      title: 'Spider-Man',
+      price: 80000,
+      description: 'bla bla bla bla bla',
+    },
+  ];
+
+  constructor() {}
+
+  getAllProducts() {
+    return this.products;
+  }
+
+  getProduct(id: string) {
+    return this.products.find((item) => id === item.id);
+  }
+}
