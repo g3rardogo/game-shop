b78257b86e021880956a91628717ca18986e8a91
Gerardo Ramirez <gerardogo145@gmail.com>
2021-10-07 20:43:54 +0200

    :art: Add Material module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductsRoutingModule } from './products.routing.module';
-
+import { MaterialModule } from '../material/material.module';
 @NgModule({
   declarations: [ProductComponent, ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [CommonModule, ProductsRoutingModule, MaterialModule],
 })
 export class ProductsModule {}
