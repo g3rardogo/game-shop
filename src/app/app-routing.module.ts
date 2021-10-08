7331e73a9bcd0db7298e45c7d344da8023dcded6
Gerardo Ramirez <gerardogo145@gmail.com>
2021-10-08 21:29:29 +0200

    :art: Add admin view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,11 @@ const routes: Routes = [
       },
     ],
   },
-
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((module) => module.AdminModule),
+  },
   {
     path: '**',
     loadChildren: () =>
